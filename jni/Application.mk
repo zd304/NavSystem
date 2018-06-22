ce6b92c7d58de1ba9b6376d5cea1d149d561055f
zd304 <568892758@qq.com>
2018-06-22 10:13:43 +0800

    fix

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,7 @@
 APP_PLATFORM	:= android-10
-APP_ABI			:= armeabi-v7a
-#APP_STL		:= stlport_static
-APP_STL			:= gnustl_static
+#APP_ABI			:= armeabi-v7a
+#APP_ABI			:= x86
+APP_ABI			:= armeabi
+APP_STL			:= stlport_static
+#APP_STL		:= gnustl_static
 APP_CPPFLAGS	:= -std=c++11
